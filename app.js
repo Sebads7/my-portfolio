5252eebcd5a1955b381a7b28017d8e6c4425771f
Sebas <disalvatore.sebastian@gmail.com>
2023-09-19 09:55:47 -0300

    added new features to site

--- app.js
+++ app.js
@@ -48,13 +48,53 @@ sticky();
 navSlide();
 
 
-var containerEl = document.querySelector(".portfolio-item");
-// 2. Passing the configuration object inline
-//https://www.kunkalabs.com/mixitup/docs/configuration-object/
-var mixer = mixitup(containerEl, {
-  animation: {
-    effects: "fade translateZ(-100px)",
-    effectsIn: "fade translateY(-100%)",
-    easing: "cubic-bezier(0.645, 0.045, 0.355, 1)"
+// var containerEl = document.querySelector(".portfolio-item");
+// // 2. Passing the configuration object inline
+// //https://www.kunkalabs.com/mixitup/docs/configuration-object/
+// var mixer = mixitup(containerEl, {
+//   animation: {
+//     effects: "fade translateZ(-100px)",
+//     effectsIn: "fade translateY(-100%)",
+//     easing: "cubic-bezier(0.645, 0.045, 0.355, 1)"
+//   }
+// });
+
+
+
+
+
+const setSubmit = () => {
+    const nameInp1 = document.getElementById("input1");
+    const nameInp2 = document.getElementById("input2");
+    const nameInp3 = document.getElementById("input3");
+
+    const submitBtn = document.querySelector(".form-botton");
+  
+    submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+  
+      if (!nameInp1.value) {
+        nameInp1.classList.add("error");
+        nameInp1.parentElement.classList.add("error_message1");
+      } else {
+        nameInp1.classList.remove("error");
+        nameInp1.parentElement.classList.remove("error_message1");
+      }
+      if (!nameInp2.value) {
+        nameInp2.classList.add("error");
+        nameInp2.parentElement.classList.add("error_message2");
+      } else {
+        nameInp2.classList.remove("error");
+        nameInp2.parentElement.classList.remove("error_message2");
+      }
+      if (!nameInp3.value) {
+        nameInp3.classList.add("error");
+        nameInp3.parentElement.classList.add("error_message3");
+      } else {
+        nameInp3.classList.remove("error");
+        nameInp3.parentElement.classList.remove("error_message3");
+      }
+    });
+  
   }
-});
+  setSubmit();
