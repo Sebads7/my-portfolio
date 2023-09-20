3d0c83e44b36d1179a8d6b432657acc0a67e5090
Sebas <disalvatore.sebastian@gmail.com>
2023-09-20 17:43:33 -0300

    changed colors of site

--- app.js
+++ app.js
@@ -98,3 +98,18 @@ const setSubmit = () => {
   
   }
   setSubmit();
+
+
+  // const forms = document.forms;
+  // if (forms.length){
+  //   for (const form of forms){
+  //     const file = form.querySelector('input[type="file"]');
+  //     file ? file.addEventListener('change', formAddFile) : null;
+  //   }
+  // }
+
+  // function formAddFile(e){
+  //   const formInputFile = e.target;
+  //   const formFiles = formInputFile.files;
+  //   const fileNames = formFiles.length ? Object.keys(formFiles).map(key => formFiles[key].name) : null;
+  // }
